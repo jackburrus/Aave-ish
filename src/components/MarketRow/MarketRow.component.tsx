6479e1d74897a39b882f7a0cd14a277e3d8e57bc
Jack Burrus <jhburrus3@gmail.com>
2021-05-08 14:48:28 -0600

    Market Row looking good

--- src/components/MarketRow/MarketRow.component.tsx
+++ src/components/MarketRow/MarketRow.component.tsx
@@ -1,12 +1,38 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { StyledMarketRowContainer } from "./MarketRow.styles";
+import { IconContainer } from "../ActionButton/ActionButton.styles";
+import {
+  StyledDetailsContainer,
+  StyledIconContainer,
+  StyledMarketRowContainer,
+  StyledMarketSizeContainer,
+  StyledAPYContainer,
+  StyledMarketSizeText,
+  StyledMarketRowValue,
+} from "./MarketRow.styles";
 interface MarketRowProps {}
 
 export const MarketRow = (props: MarketRowProps) => {
   return (
     <StyledMarketRowContainer>
-      <Text>Coin Data</Text>
+      <StyledIconContainer>
+        <IconContainer
+          style={{ width: 70, height: 70 }}
+          shadowColor={"rgba(108, 218, 176, 0.6)"}
+        >
+          <Text>Icon</Text>
+        </IconContainer>
+      </StyledIconContainer>
+      <StyledDetailsContainer>
+        <StyledMarketSizeContainer>
+          <StyledMarketSizeText>Market Size</StyledMarketSizeText>
+          <StyledMarketRowValue>892.67M</StyledMarketRowValue>
+        </StyledMarketSizeContainer>
+        <StyledAPYContainer>
+          <StyledMarketSizeText>Deposit APY</StyledMarketSizeText>
+          <StyledMarketRowValue>12.63%</StyledMarketRowValue>
+        </StyledAPYContainer>
+      </StyledDetailsContainer>
     </StyledMarketRowContainer>
   );
 };
--- src/components/MarketRow/MarketRow.styles.ts
+++ src/components/MarketRow/MarketRow.styles.ts
@@ -10,11 +10,54 @@ export const StyledMarketRowContainer = styled.View`
   border-radius: 20px;
   background-color: ${(props) => props.theme.lightGreen};
   margin-top: 20px;
+  flex-direction: row;
 `;
 
 export const StyledMarketsContainer = styled.View`
   width: ${width}px;
   display: flex;
   flex: 1;
-  border: 3px solid green;
+`;
+
+export const StyledIconContainer = styled.View`
+  /* border: 1px solid black; */
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledDetailsContainer = styled.View`
+  /* border: 1px solid black; */
+  display: flex;
+  flex: 2;
+  flex-direction: row;
+`;
+export const StyledMarketSizeContainer = styled.View`
+  /* border: 1px solid black; */
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+export const StyledAPYContainer = styled.View`
+  /* border: 1px solid black; */
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledMarketSizeText = styled.Text`
+  color: ${(props) => props.theme.main};
+  font-size: 16px;
+  font-family: "Rubik_500Medium";
+  padding-bottom: 5px;
+`;
+
+export const StyledMarketRowValue = styled.Text`
+  color: ${(props) => props.theme.main};
+  font-size: 14px;
+  font-family: "Rubik_500Medium";
+  opacity: 0.8;
 `;
