3cc8f43ba47c3b16516d5b0bf9b830511f7910b1
Jack <jhburrus3@gmail.com>
2021-05-08 10:14:09 -0600

    Card organization setup

--- src/components/WalletCard/WalletCard.component.tsx
+++ src/components/WalletCard/WalletCard.component.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-import { WalletCardContainer } from "./WalletCard.styles";
+import {
+  WalletBalanceContainer,
+  WalletCardContainer,
+  WalletCardTitleContainer,
+  TitleAndBalanceContainer,
+  WalletChartContainer,
+} from "./WalletCard.styles";
 interface WalletCardProps {}
 
 export const WalletCard = (props: WalletCardProps) => {
-  return <WalletCardContainer />;
+  return (
+    <WalletCardContainer>
+      <TitleAndBalanceContainer>
+        <WalletCardTitleContainer>
+          <Text>Title</Text>
+        </WalletCardTitleContainer>
+        <WalletBalanceContainer>
+          <Text>Balance</Text>
+        </WalletBalanceContainer>
+      </TitleAndBalanceContainer>
+      <WalletChartContainer>
+        <Text>Chart</Text>
+      </WalletChartContainer>
+    </WalletCardContainer>
+  );
 };
--- src/components/WalletCard/WalletCard.styles.ts
+++ src/components/WalletCard/WalletCard.styles.ts
@@ -4,9 +4,37 @@ import styled from "styled-components/native";
 const { width, height } = Dimensions.get("window");
 
 export const WalletCardContainer = styled.View`
-  /* border: 1px solid red; */
+  flex-direction: row;
   width: ${width - 50}px;
   height: 150px;
   border-radius: 30px;
   background-color: ${(props) => props.theme.lightGreen};
+`;
+export const WalletCardTitleContainer = styled.View`
+  /* border-top-left-radius: 30px;
+  border-top-right-radius: 30px; */
+  display: flex;
+  flex: 1;
+  border: 1px solid black;
+`;
+
+export const WalletBalanceContainer = styled.View`
+  display: flex;
+  flex: 1;
+  border: 1px solid purple;
+`;
+export const WalletChartContainer = styled.View`
+  width: ${width - 50}px;
+  display: flex;
+  flex: 1;
+  border: 1px solid black;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TitleAndBalanceContainer = styled.View`
+  border: 1px solid red;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
 `;
