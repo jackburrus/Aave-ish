4a97f6347632326e9fe4cc7cd46d727d5edfe09c
Jack <jhburrus3@gmail.com>
2021-05-10 18:07:21 -0600

    Opacity technically working add icons

--- src/components/WalletCard/PieChartIcon.Component.tsx
+++ src/components/WalletCard/PieChartIcon.Component.tsx
@@ -1,17 +1,43 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Dimensions } from "react-native";
 import { View, Text, StyleSheet } from "react-native";
+import Animated, {
+  useSharedValue,
+  Extrapolate,
+  useAnimatedStyle,
+  withDelay,
+  withTiming,
+  interpolate,
+  Easing,
+} from "react-native-reanimated";
 import { ChartContext } from "../../context/ChartContext";
 
 interface PieChartIconProps {}
 
-const { width, height } = Dimensions.get("window");
+const { width } = Dimensions.get("window");
 
 export const PieChartIcon = (props: PieChartIconProps) => {
   const { chartIndex } = useContext(ChartContext);
+  const iconOpacity = useSharedValue(0);
+  const opacityProgress = useSharedValue(0);
+  useEffect(() => {
+    opacityProgress.value = 0;
+    opacityProgress.value = withTiming(1, {
+      duration: 650,
+      easing: Easing.inOut(Easing.ease),
+    });
+    console.log("ran index");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [chartIndex]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacityProgress.value,
+  }));
   return (
-    <Text style={{ position: "absolute", right: width / 4 + 2 }}>
-      {chartIndex}
-    </Text>
+    <Animated.View
+      style={[{ position: "absolute", right: width / 4 + 2 }, animatedStyle]}
+    >
+      <Text>{chartIndex}</Text>
+    </Animated.View>
   );
 };
