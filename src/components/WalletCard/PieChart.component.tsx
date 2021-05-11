6427acbd8226fb94e08318a6f58ebe09b1993502
Jack <jhburrus3@gmail.com>
2021-05-10 18:43:28 -0600

    Change colors with icon

--- src/components/WalletCard/PieChart.component.tsx
+++ src/components/WalletCard/PieChart.component.tsx
@@ -9,7 +9,7 @@ const PieChartExample = () => {
     {
       key: 1,
       value: 50,
-      svg: { fill: "#600080" },
+      svg: { fill: chartIndex == 1 ? "#8A92B2" : "#600080" },
       onPress: () => {
         setChartIndex(1);
       },
@@ -18,7 +18,7 @@ const PieChartExample = () => {
     {
       key: 2,
       value: 50,
-      svg: { fill: "#9900cc" },
+      svg: { fill: chartIndex === 2 ? "#36B4C4" : "#9900cc" },
       onPress: () => setChartIndex(2),
       arc: chartIndex === 2 ? { outerRadius: "130%", cornerRadius: 10 } : null,
       // arc: { outerRadius: "130%", cornerRadius: 10 },
@@ -26,21 +26,22 @@ const PieChartExample = () => {
     {
       key: 3,
       value: 40,
-      svg: { fill: "#c61aff" },
+      svg: { fill: chartIndex === 3 ? "#FF007A" : "#c61aff" },
       onPress: () => setChartIndex(3),
       arc: chartIndex === 3 ? { outerRadius: "130%", cornerRadius: 10 } : null,
     },
     {
       key: 4,
       value: 95,
-      svg: { fill: "#d966ff" },
+      svg: { fill: chartIndex === 4 ? "#00D395" : "#ecb3ff" },
       onPress: () => setChartIndex(4),
       arc: chartIndex === 4 ? { outerRadius: "130%", cornerRadius: 10 } : null,
     },
     {
       key: 5,
       value: 35,
-      svg: { fill: "#ecb3ff" },
+      svg: { fill: chartIndex === 5 ? "#AF71BD" : "#d966ff" },
+
       onPress: () => setChartIndex(5),
       arc: chartIndex === 5 ? { outerRadius: "130%", cornerRadius: 10 } : null,
     },
