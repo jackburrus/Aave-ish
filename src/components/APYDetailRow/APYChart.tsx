6c7784a865e3768115c30b19ad260dc898a44edc
Jack Burrus <jhburrus3@gmail.com>
2021-05-12 11:34:53 -0600

    percentage change looking good

--- src/components/APYDetailRow/APYChart.tsx
+++ src/components/APYDetailRow/APYChart.tsx
@@ -12,23 +12,6 @@ import { useTheme } from "styled-components";
 import { APYChartLabel } from "./APYChartLabel";
 export const { width: SIZE } = Dimensions.get("window");
 
-export const data = [
-  { x: 1453075200, y: 1.47 },
-  { x: 1453161600, y: 1.37 },
-  { x: 1453248000, y: 1.53 },
-  { x: 1453334400, y: 1.54 },
-  { x: 1453420800, y: 1.52 },
-  { x: 1453507200, y: 2.03 },
-  { x: 1453593600, y: 2.1 },
-  { x: 1453680000, y: 2.5 },
-  { x: 1453766400, y: 2.3 },
-  { x: 1453852800, y: 2.42 },
-  { x: 1453939200, y: 2.55 },
-  { x: 1454025600, y: 2.41 },
-  { x: 1454112000, y: 2.43 },
-  { x: 1454198400, y: 2.2 },
-];
-
 const getData = (type, subtitle) => {
   if (type === "Deposit APY") {
     return [
@@ -84,16 +67,16 @@ const getData = (type, subtitle) => {
   }
 };
 
-const getY = (value, defaultValue) => {
-  "worklet";
-
-  return !value ? defaultValue : `${value.substring(0, 4)} %`;
-};
 const APYChart = (props) => {
   const { main, liquidGreen } = useTheme();
   const { type, subtitle, defaultValue } = props;
   const data = getData(type, subtitle);
   const points = monotoneCubicInterpolation({ data, range: 40 });
+  const getY = (value) => {
+    "worklet";
+
+    return !value ? defaultValue : `${value.substring(0, 4)} %`;
+  };
   return (
     <View
       style={{
@@ -122,7 +105,7 @@ const APYChart = (props) => {
             color: main,
             opacity: 0.8,
           }}
-          format={(value) => getY(value, defaultValue)}
+          format={getY}
         />
       </ChartPathProvider>
     </View>
